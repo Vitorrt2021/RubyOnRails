b2bceddc20bdf9ff8defccb4e5cff3cd3a031157
Vitor Ramos Turibio <vitorrt2015@gmail.com>
2022-04-18 15:47:50 -0300

    Fix: refactoring using DRY in project_1

--- course/2_RubyOnRails/project_1/app/controllers/articles_controller.rb
+++ course/2_RubyOnRails/project_1/app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+
     def show 
         # debugger
-        @article = Article.find(params[:id])
     end
 
     def index 
@@ -14,7 +15,7 @@ class ArticlesController < ApplicationController
 
     def create
     
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(article_params)
         # render :json => params[:article]
         if @article.save
             # render plain: @article.inspect
@@ -27,12 +28,10 @@ class ArticlesController < ApplicationController
     end
 
     def edit 
-        @article = Article.find(params[:id])
         # debugger
     end
     def update
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description))
+        if @article.update(article_params)
             flash[:notice] = 'Article was update successfully'
             redirect_to @article
         else    
@@ -43,8 +42,17 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy()
         redirect_to article_path
+    end
+
+    private 
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :description)
     end
 end
