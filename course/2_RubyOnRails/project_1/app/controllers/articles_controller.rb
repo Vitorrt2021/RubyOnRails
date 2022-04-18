5aae263a2299aee354173eded59e59eb30895976
Vitor Ramos Turibio <vitorrt2015@gmail.com>
2022-04-18 10:32:01 -0300

    Feat: adding update route in project_1

--- course/2_RubyOnRails/project_1/app/controllers/articles_controller.rb
+++ course/2_RubyOnRails/project_1/app/controllers/articles_controller.rb
@@ -7,4 +7,38 @@ class ArticlesController < ApplicationController
     def index 
         @articles = Article.all   
     end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+    
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        # render :json => params[:article]
+        if @article.save
+            # render plain: @article.inspect
+            flash[:notice] = 'Article was created successfully'
+            redirect_to article_path(@article)
+            # redirect_to @article
+        else 
+            render 'new'
+        end
+    end
+
+    def edit 
+        @article = Article.find(params[:id])
+        # debugger
+    end
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = 'Article was update successfully'
+            redirect_to @article
+        else    
+            render 'edit'
+
+        end
+        # debugger
+    end
 end
